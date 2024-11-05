4aea8c95506e9f20d2f2c2715f31c4e6dbc747c5
Mehmet <mehmetext68@gmail.com>
2024-11-05 18:19:47 +0300

    Update example.tsx

--- src/screens/example.tsx
+++ src/screens/example.tsx
@@ -3,7 +3,8 @@ import PageContainer from "../components/page-container";
 export default function ExampleScreen() {
   return (
     <PageContainer appBar={{ title: "Example" }}>
-      <div>ExampleScreen</div>
+      <div className="aspect-square bg-green-500 dark:bg-red-500">ads</div>
+      <div className="aspect-square bg-red-500 dark:bg-green-500">ads</div>
     </PageContainer>
   );
 }

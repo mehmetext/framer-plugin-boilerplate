696489f92a48d0229e692004c7ae86949ebec803
Mehmet <mehmetext68@gmail.com>
2024-11-07 05:36:51 +0300

    feat: add classNames to page container

--- src/components/page-container.tsx
+++ src/components/page-container.tsx
@@ -1,19 +1,31 @@
 import React from "react";
 import AppBar, { AppBarProps } from "./app-bar";
+import { cn } from "../lib/utils";
 
-interface PageContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+interface PageContainerProps {
   appBar?: AppBarProps;
+  children?: React.ReactNode;
+  className?: string;
+  containerClassName?: string;
 }
 
 export default function PageContainer({
   children,
   appBar,
-  ...props
+  className,
+  containerClassName,
 }: PageContainerProps) {
   return (
-    <div className="flex flex-col" {...props}>
+    <div className={cn("flex-1 flex flex-col", className)}>
       {appBar && <AppBar {...appBar} />}
-      <main className="flex-1 flex flex-col gap-2 px-2 pb-2">{children}</main>
+      <main
+        className={cn(
+          "flex-1 flex flex-col gap-2 px-2 pb-2",
+          containerClassName
+        )}
+      >
+        {children}
+      </main>
     </div>
   );
 }
